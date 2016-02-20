3381817476f7f574e8aaafef195987e8631793fc
Václav Kraus <krauva@seznam.cz>
2016-02-20 17:55:46 +0100

    Added reflection class

--- src/Panel.php
+++ src/Panel.php
@@ -16,42 +16,49 @@ use Nette;
 
 class Panel extends Nette\Object implements Tracy\IBarPanel
 {
+    private $container, $reflection;
 
-	/** @var string */
-	private $dir;
+    /**
+     * Panel constructor.
+     * @param $container
+     */
+    public function __construct($container)
+    {
+        $this->container = $container;
 
-	private $container;
+        if ($this->container->hasService('authorizator')) {
+			$this->reflection = new Reflection($this->container->getByType('Nette\Security\Permission'));
+        }
+    }
 
-	public function __construct($container)
-	{
-		$this->container = $container;
-	}
+    /**
+     * Renders HTML code for custom tab.
+     * @return string
+     */
+    function getTab()
+    {
+        if ($this->container->hasService('authorizator')) {
+            ob_start();
+            require __DIR__ . '/templates/Panel.tab.phtml';
+            return ob_get_clean();
+        }
+    }
 
-	/**
-	 * Renders HTML code for custom tab.
-	 * @return string
-	 */
-	function getTab()
-	{
-		ob_start();
-		require __DIR__ . '/templates/Panel.tab.phtml';
-		return ob_get_clean();
-	}
+    /**
+     * Renders HTML code for custom panel.
+     * @return string
+     */
+    function getPanel()
+    {
+        if ($this->container->hasService('authorizator')) {
+            ob_start();
+            $Permission = $this->container->getByType('Nette\Security\Permission');
+            $permissions = $this->reflection->getPropertyValue('rules');
+            $resources = $Permission->getResources();
+            $roles = $Permission->getRoles();
 
-	/**
-	 * Renders HTML code for custom panel.
-	 * @return string
-	 */
-	function getPanel()
-	{
-		ob_start();
-
-		$Permission = $this->container->getByType('Nette\Security\Permission');
-		$permissions = (array)$Permission;
-		$resources = $Permission->getResources();
-		$roles = $Permission->getRoles();
-
-		require __DIR__ . '/templates/Panel.panel.phtml';
-		return ob_get_clean();
-	}
+            require __DIR__ . '/templates/Panel.panel.phtml';
+            return ob_get_clean();
+        }
+    }
 }
